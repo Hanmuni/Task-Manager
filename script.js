7889b4a6f5e281f476056274091d5149aba0977c
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2022-06-03 01:39:01 +0200

    integrate smallest_backend_ever

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
- /* Alle */
- let tasks = [];
+/* Alle */
+let tasks = [];
 
- async function init() {
-     await includeHTML();
-     loadTasks();
- }
+async function init() {
+    await includeHTML();
 
- async function includeHTML() {
-     let includeElements = document.querySelectorAll('[w3-include-html]');
+    await downloadFromServer();
 
-     for (let i = 0; i < includeElements.length; i++) {
-         const element = includeElements[i];
-         file = element.getAttribute("w3-include-html");
+    tasks = JSON.parse(backend.getItem('tasks')) || [];
+    console.log(tasks);
+}
 
-         let response = await fetch(file);
-         if (response.ok) {
-             element.innerHTML = await response.text();
-         } else {
-             element.innerHTML = 'Page not found';
-         }
-     }
- }
- /* Alle */
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
 
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
 
- /* Ole */
-
- /* Ole*/
-
-
- /* Fabian */
-
- /* Fabian*/
+        let response = await fetch(file);
+        if (response.ok) {
+            element.innerHTML = await response.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+/* Alle */
 
 
- /* Hong Hanh */
- function loadTasks() {
-     tasks = JSON.parse(localStorage.getItem('task')) || [];
-     console.log('Loading Complete', tasks);
- }
+/* Ole */
 
- function createTask() {
-     let title = document.getElementById('title').value;
-     let category = document.getElementById('category').value;
-     let description = document.getElementById('description').value;
-     let urgency = document.getElementById('urgency').value;
+/* Ole*/
 
-     let task = {
-         'title': title,
-         'category': category,
-         'description': description,
-         'date': new Date().getTime(),
-         'urgency': urgency,
-     };
 
-     tasks.push(task);
+/* Fabian */
 
-     document.getElementById('title').value = ``;
-     document.getElementById('description').value = ``;
+/* Fabian*/
 
-     let tasksAsString = JSON.stringify(tasks);
-     localStorage.setItem('tasks', tasksAsString);
- }
 
- function cancelTask() {
-     document.getElementById('title').value = ``;
-     document.getElementById('description').value = ``;
- }
+/* Hong Hanh */
+function createTask() {
+    let title = document.getElementById('title').value;
+    let category = document.getElementById('category').value;
+    let description = document.getElementById('description').value;
+    let urgency = document.getElementById('urgency').value;
 
- /* Hong Hanh*/
+    let task = {
+        'title': title,
+        'category': category,
+        'description': description,
+        'date': new Date().getTime(),
+        'urgency': urgency,
+    };
+
+    tasks.push(task);
+
+    document.getElementById('title').value = ``;
+    document.getElementById('description').value = ``;
+
+    let tasksAsString = JSON.stringify(tasks);
+    backend.setItem('tasks', tasksAsString);
+
+    init();
+}
+
+function cancelTask() {
+    document.getElementById('title').value = ``;
+    document.getElementById('description').value = ``;
+}
+/* Hong Hanh*/
