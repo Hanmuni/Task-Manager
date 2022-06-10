26d0a69cc85e9b3449ae4bc60e2a162496aaf492
Blutwurst89 <101867623+Blutwurst89@users.noreply.github.com>
2022-06-10 19:19:04 +0800

    Update board.js
    Drag and Drop works

--- board.js
+++ board.js
@@ -1,47 +1,63 @@
 let todos = [{
     'id': 0,
-    'title': 'Coden',
-    'category': 'todo',
+    'title': 'planen',
+    'status': 'todo',
 }, {
     'id': 1,
-    'title': 'Planen',
-    'category': 'inprogress',
+    'title': 'coden',
+    'status': 'inprogress',
+}, {
+    'id': 2,
+    'title': 'fine tuning',
+    'status': 'testing',
+}, {
+    'id': 3,
+    'title': 'chillen',
+    'status': 'done',
 }];
 
 let currentDraggedElement;
 
+async function init_board() {
+    setURL('http://gruppe-247.developerakademie.net/smallest_backend_ever');
+    await includeHTML();
+    await loadAllTasks();
+    load_current_user_local();
+    await updateHTML();
+}
+
 function updateHTML() {
 
-    let todo = todos.filter(t => t['category'] == 'todo');
+    let todo = todos.filter(t => t['status'] == 'todo');
 
-    document.getElementbyID('todo').innerHTML = '';
+    document.getElementById('todo').innerHTML = '';
 
     for (let index = 0; index < todo.length; index++) {
         const element = todo[index];
         document.getElementById('todo').innerHTML += generateTodoHTML(element);
     }
 
-    let inprogress = todos.filter(t => t['category'] == 'inprogress');
+    let inprogress = todos.filter(t => t['status'] == 'inprogress');
 
-    document.getElementbyID('inprogress').innerHTML = '';
+    document.getElementById('inprogress').innerHTML = '';
 
     for (let index = 0; index < inprogress.length; index++) {
         const element = inprogress[index];
         document.getElementById('inprogress').innerHTML += generateTodoHTML(element);
     }
 
-    let testing = todos.filter(t => t['category'] == 'testing');
+    let testing = todos.filter(t => t['status'] == 'testing');
 
-    document.getElementbyID('testing').innerHTML = '';
+    document.getElementById('testing').innerHTML = '';
 
     for (let index = 0; index < testing.length; index++) {
         const element = testing[index];
         document.getElementById('testing').innerHTML += generateTodoHTML(element);
     }
 
-    let done = todos.filter(t => t['category'] == 'done');
+    let done = todos.filter(t => t['status'] == 'done');
 
-    document.getElementbyID('done').innerHTML = '';
+    document.getElementById('done').innerHTML = '';
 
     for (let index = 0; index < done.length; index++) {
         const element = done[index];
